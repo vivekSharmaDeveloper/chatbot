583a2b2111df073c2193b832163545e0d5182e03
vivekSharmaDeveloper <lingola098@gmail.com>
2024-11-24 20:49:45 +0530

    Database Configuration

---
+++ backend/src/model/Chat.modal.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { randomUUID } from "crypto";
+
+const chatSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    default: randomUUID(),
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+});
+const Chat= mongoose.model("Chat", chatSchema);
+
+export default Chat;
---
+++ backend/src/model/User.modal.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const userSchema = new mongoose_1.default.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    chats: [{
+            type: mongoose_1.default.Schema.Types.ObjectId,
+            ref: "Chat",
+        }],
+});
+exports.default = mongoose_1.default.model("User", userSchema);
